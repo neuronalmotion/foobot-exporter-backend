ec04e411f09ceb24aaed84a747ab3eb1934fc7fe
Robin Penea <robin.penea@gmail.com>
2016-11-17 16:52:18 +0100

    Redirect root to app

--- foobot_exporter/urls.py
+++ foobot_exporter/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 from rest_framework import routers
 from .views import OwnerViewSet, DeviceViewSet
@@ -11,4 +12,5 @@ router.register(r'devices', DeviceViewSet, base_name='device')
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls, namespace='api')),
+    url(r'^$', RedirectView.as_view(url='/app/')),
 ]
