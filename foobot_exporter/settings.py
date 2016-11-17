b614d3f047aec47aa0b5aa5db032eef7dde939df
Robin Penea <robin.penea@gmail.com>
2016-11-17 16:05:34 +0100

    Add static path in settings

--- foobot_exporter/settings.py
+++ foobot_exporter/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 FOOBOT_API_BASE_URL = 'http://api.foobot.io/v2'
 
